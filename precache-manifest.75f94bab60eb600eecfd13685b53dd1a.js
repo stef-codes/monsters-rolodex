b694291d98869ae0eeef6e6c03062e66099dfde7
Stefanie <stefcodes@gmail.com>
2020-03-06 01:41:28 -0800

    Updates

---
+++ precache-manifest.75f94bab60eb600eecfd13685b53dd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94338f360c8f7140822ffe5b3be774c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2fdf9d3e66a5b5a849f4",
+    "url": "/monsters-rolodex/static/css/main.f511f7e0.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdf9d3e66a5b5a849f4",
+    "url": "/monsters-rolodex/static/js/main.0dd5b161.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
